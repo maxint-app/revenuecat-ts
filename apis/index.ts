f5b2c4846c4e8a52bd80bc9e2ef9c0dd3f4936a7
Kingkor Roy Tirtho <krtirtho@maxint.com>
2024-03-28 11:04:59 +0600

    feat: revenuecat v2 generated

---
+++ apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppApi';
+export * from './ChartsMetricsApi';
+export * from './CustomerApi';
+export * from './EntitlementApi';
+export * from './InvoiceApi';
+export * from './OfferingApi';
+export * from './PackageApi';
+export * from './ProductApi';
+export * from './ProjectApi';
+export * from './SubscriptionApi';
